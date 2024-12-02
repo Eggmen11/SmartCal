135aeada5a57a6b9aeadad4f7ccccea750fbd4c2
Egi <egi.albayrak@gmail.com>
2024-12-02 21:07:50 +0100

    Add anthropic llm setup file

--- app.mjs
+++ app.mjs
@@ -5,6 +5,8 @@ import path from 'path';
 const app = express();
 
 app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ limit: '50mb' }));
 
 
 app.set('view engine', 'ejs');
@@ -13,3 +15,7 @@ app.listen(3000);
 
 app.get("/", (req, res) => res.render("home"));
 app.get("/calc", (req, res) => res.render("calculator"));
+
+app.post("/upload", (req, res) => {
+    console.log(req.body);
+});
---
+++ services/llmService.mjs
@@ -0,0 +1,62 @@
+import Anthropic from "@anthropic-ai/sdk";
+
+const client = new Anthropic({
+	apiKey: "my_api_key", // defaults to process.env["ANTHROPIC_API_KEY"]
+});
+
+const systemPrompt = `
+        You are an expert computer vision system. First describe the image in accurate details, then analyze the provided images and return ONLY a JSON object containing bounding boxes. Be super precise and try to detect as many objects as possible.
+        Be accurate and try to detect as many objects as possible. Really open your eyes and see the world.
+
+        Follow these strict rules:
+        1. Output MUST be valid JSON with no additional text
+        2. Each detected object must have:
+            - 'element': descriptive name of the object
+            - 'bbox': [x1, y1, x2, y2] coordinates (normalized 0-1)
+            - 'confidence': confidence score (0-1)
+        3. Use this exact format:
+        {
+            "image_number": [
+                {
+                "element": "object_name",
+                "bbox": [x1, y1, x2, y2],
+                "confidence": 0.95
+                }
+            ]
+        }
+        4. Coordinates must be precise and properly normalized
+        5. DO NOT include any explanation or additional text
+        """
+
+`;
+
+export async function getBorderedImg(imageData) {
+	const base64Image = imageData.image;
+	const type = imageData.type;
+	const content = [];
+
+	content.push({
+		type: "image",
+		source: {
+			type: "base64",
+			media_type: type,
+			data: base64Image,
+		},
+	});
+
+	try {
+		const response = await client.messages.create({
+			model: "claude-3-5-sonnet-20241022",
+			max_tokens: 8000,
+			system: systemPrompt,
+			messages: [
+				{
+					role: "user",
+					content: content,
+				},
+			],
+		});
+	} catch (error) {
+		console.log(error);
+	}
+}
